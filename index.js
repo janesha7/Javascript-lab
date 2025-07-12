be1f18b7406ce7625b73061dde2b5cd7144e0867
Janesha Shree <24bct124@kgcas.com>
2025-07-12 18:59:58 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+//task1
+var number = 10;
+var name = 'jhon';
+var iscloudy = true;
+console.log(number);
+console.log(name);
+console.log(iscloudy);
+//task2
+var a =10;
+var b = 13;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+console.log(a%b);
+//task3
+var num1 = prompt("Enter the val1");
+var num2 = prompt("Enter the val2");
+console.log("sum",num1+num2);
