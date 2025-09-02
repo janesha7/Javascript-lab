59c90b8a05e3e16979b6b7267816e0751f0fe369
Janesha Shree <24bct124@kgcas.com>
2025-09-02 14:21:59 +0530

    Add files via upload

---
+++ day8.js
@@ -0,0 +1,38 @@
+let currenttime=new Date();
+console.log(currenttime);
+let local=currenttime.toString();
+console.log(local);
+let hours=currenttime.getHours();
+console.log(hours)
+let minits=currenttime.getMinutes();
+console.log(minits);
+let day =currenttime.getDay();
+console.log(day);
+
+class Exam{
+    constructor(personname){
+        this.personname=personname;
+        console.log(personname);
+    }
+}
+let result=new Exam("john");
+
+class Bank{
+    constructor(initialamount=0){
+     this._balance=initialamount;
+    }
+    get balance(){
+        return this._balance;
+    }
+    set balance(amountdeposit){
+        if(amountdeposit>0){
+            this.balance += amountdeposit;
+            console.log(this._balance)
+        }
+    }
+}
+let add=new Bank(10);
+console.log(add)
+add._balance=300;
+console.log(add)
+console.log(add._balance)
